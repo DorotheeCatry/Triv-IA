0a1687006656fe870d6028229c460b51074a6f79
flavigny maxime <maxime.mcfly@gmail.com>
2024-12-16 22:05:48 +0100

    player

---
+++ player.py
@@ -0,0 +1,7 @@
+class Player:
+    def __init__(self, camember_number, run, answer, position, winner):
+        self.camember_number = camember_number
+        self.run = run
+        self.answer = answer
+        self.position = position
+        self.winner = winner
