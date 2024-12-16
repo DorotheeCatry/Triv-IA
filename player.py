818198ed669d8901ce022721ac9737c3bbce2f7d
flavigny maxime <maxime.mcfly@gmail.com>
2024-12-17 00:00:28 +0100

    player2

--- player.py
+++ player.py
@@ -1,3 +1,5 @@
+import random
+
 class Player:
     def __init__(self, camember_number, run, answer, position, winner):
         self.camember_number = camember_number
@@ -5,3 +7,25 @@ class Player:
         self.answer = answer
         self.position = position
         self.winner = winner
+
+    
+    def win_a_camembert(self, camembert_number):
+        self.camember_number + 1
+
+
+    def dice(self):
+        return random.randint(1, 6)    
+
+
+    def nouvelle_position(self, position, direction ):
+        if direction == "avant":
+            self.position += dice_value
+        elif direction == "arriere":
+            self.position -= dice_value
+        self.position %= 42       
+        
+        
+
+    
+        
+
